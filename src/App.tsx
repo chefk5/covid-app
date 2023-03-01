1a09d52f2ea772c1f85d3225fe8a17dbf6948646
Khaledc <kncharkie@gmail.com>
2023-03-01 13:33:32 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,14 @@ function App() {
   }
 
   return (
-    ss
     loading ? <p>loading</p>
-    : <Box >
-      <ToggleColor />
-      <CountrySelector data={data} selectCountry={selectCountry} />
-      <Dashboard data={data} selectedCountry={selectedCountry} />
-      {/* {loading ? <Text>Loading...</Text> : <p>not Loading...</p>} */}
+      : <Box >
+        <ToggleColor />
+        <CountrySelector data={data} selectCountry={selectCountry} />
+        <Dashboard data={data} selectedCountry={selectedCountry} />
+        {/* {loading ? <Text>Loading...</Text> : <p>not Loading...</p>} */}
 
-    </Box>
+      </Box>
   )
 }
 
