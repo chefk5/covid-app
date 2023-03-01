65ebac4cf86b172bb9ab7e5aa38732bbdb67f8e0
Khaledc <kncharkie@gmail.com>
2023-03-01 12:45:01 +0200

    test

--- src/components/ReportedCases.tsx
+++ src/components/ReportedCases.tsx
@@ -51,7 +51,7 @@ const ReportedCases = ({ data, selectedCountry }: Props) => {
             </Flex>
             <Flex justify={'center'} gap={20} marginTop={10} >
                 <RadioGroup onChange={setMetric} value={metric}>
-                    <Stack direction=s>
+                    <Stack direction=ss>
 
                         <Radio value="deaths">Death count</Radio>
                         <Radio value="cases">Confirmed cases</Radio>
